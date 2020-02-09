34c43218ee8042776a2f24136a6a66fd5ef9580d
maccadley <>
2020-02-09 18:57:06 +0300

    Started a new project

---
+++ StarWarsChars/StarWarsChars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarsChars
+//
+//  Created by Admin on 09.02.2020.
+//  Copyright © 2020 MaximMasov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
