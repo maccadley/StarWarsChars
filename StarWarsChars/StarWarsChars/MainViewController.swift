5837f5f4d3b87ff1e632cac7a22cd3393a6442e5
maccadley <>
2020-02-09 20:21:16 +0300

    Search bar

--- StarWarsChars/StarWarsChars/MainViewController.swift
+++ StarWarsChars/StarWarsChars/MainViewController.swift
@@ -13,18 +13,34 @@ class MainViewController: UIViewController {
 
     @IBOutlet weak var userSearchbar: UISearchBar!
     @IBOutlet weak var nameTextLabel: UILabel!
-    
+    let searchController = UISearchController(searchResultsController: nil)
     override func viewDidLoad() {
         super.viewDidLoad()
         request("https://swapi.co/api/people/1/").responseJSON { (response) in
             print(response)
         }
-        
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Write Character's name"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
         
     }
+    
+    var isSearchBarEmpty: Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    
+    
 
     
     @IBAction func searchButton(_ sender: Any) {
     }
 }
 
+extension MainViewController: UISearchResultsUpdating{
+    func updateSearchResults(for searchController: UISearchController){
+        
+    }
+}
